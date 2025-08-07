1105367bad04f8d7324268b474dbadfcdfde148c
Aditya-s14 <aditya10wasd@gmail.com>
2025-08-07 20:47:37 +0530

    data ingestion

--- src/exception.py
+++ src/exception.py
@@ -1,22 +1,22 @@
 import sys
-import logging
-from src.logger import logging  # Assuming logger is correctly defined in src/logger.py
+from src.logger import logging
 
 def error_message_detail(error, error_detail: sys):
-    _, _, exc_tb = error_detail.exc_info()
-    file_name = exc_tb.tb_frame.f_code.co_filename
-    error_message = "Error occurred in python script name [{0}] line number [{1}] error message [{2}]".format(
-        file_name, exc_tb.tb_lineno, str(error))
-    return error_message
+    try:
+        _, _, exc_tb = error_detail.exc_info()
+        file_name = exc_tb.tb_frame.f_code.co_filename if exc_tb else "Unknown"
+        line_no = exc_tb.tb_lineno if exc_tb else "Unknown"
+        error_message = f"Error occurred in script [{file_name}] at line [{line_no}]: {str(error)}"
+        return error_message
+    except Exception as e:
+        return f"Failed to extract error details: {str(e)}"
 
 
 class CustomException(Exception):
     def __init__(self, error_message, error_detail: sys):
-        super().__init__(error_message)
-        self.error_message = error_message_detail(error_message, error_detail=error_detail)
-    
+        self.error_message = error_message_detail(error_message, error_detail)
+        logging.error(self.error_message)  # Log it when exception is created
+        super().__init__(self.error_message)
+
     def __str__(self):
         return self.error_message
-
-
-
